6400f0241b4aea6423125e51e16ee8e759da4e1c
석정원 <84588595+Jeongwon-Seok@users.noreply.github.com>
2021-07-30 03:00:47 +0900

    #10 FIN

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "1f3c9180f7a3608ce000e15fb3140cdc";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  console.log("You live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = data.name;
+      city.innerText = `${data.weather[0].main} ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
